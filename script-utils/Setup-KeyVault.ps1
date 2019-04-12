610e2d13ac17f4adb1fa7f6a2fbb6d534f5d3cfd
Ricardo Serradas <riserrad@microsoft.com>
2019-04-12 15:03:16 -0700

    Adjusting powershell script to be the same as source

--- script-utils/Setup-KeyVault.ps1
+++ script-utils/Setup-KeyVault.ps1
@@ -3,7 +3,7 @@ param(
 [string]$Subject,
 
 [parameter(Mandatory=$false, ParameterSetName="GenerateCertificate")]
-[string]$CertStoreLocation = ".",
+[string]$CertStoreLocation = "Cert:\CurrentUser\My",
 
 [parameter(Mandatory=$false, ParameterSetName="GenerateCertificate")]
 [DateTime]$NotBefore = [DateTime]::Now.Date,
@@ -85,8 +85,6 @@ switch($PSCmdlet.ParameterSetName)
 {
     "GenerateCertificate"
     {
-        Install-Module -Name PSPKI
-
         # There is an existing bug with PowerShell where $PSScriptRoot is not set if used as a parameter's default value.  We'll work around
         # this here by checking to see if a value was provided for $OutputPath, and if not, use the default
         if ($PSBoundParameters.ContainsKey('OutputPath'))
@@ -98,7 +96,7 @@ switch($PSCmdlet.ParameterSetName)
             $OutputFilePath = "$PSScriptRoot\$Subject.cer"
         }
 
-        $x509Cert = New-SelfSignedCertificateEx -NotBefore $NotBefore -NotAfter $NotAfter -Subject $Subject -Path $CertStoreLocation -Provider "Microsoft Strong Cryptographic Provider"
+        $x509Cert = New-SelfSignedCertificate -NotBefore $NotBefore -NotAfter $NotAfter -Subject $Subject -CertStoreLocation $CertStoreLocation -Provider "Microsoft Strong Cryptographic Provider"
 
         # Export the public key portion of the certificate
         $cerFileInfo = Export-Certificate -Cert $x509Cert -FilePath $outputFilePath
